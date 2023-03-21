8c2017b0c9969087db683a7227f0b80d8036eb78
OnlyPacheco <101291879+OnlyPacheco@users.noreply.github.com>
2023-03-21 14:27:05 -0300

    Add files via upload

---
+++ Atividade #1 ( Lista de Carros com foreach).php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <title>Atividade #1</title>
+  </head>
+  <body>
+    <?php 
+
+     echo '<h1>Atividade #1</h1>';
+     $carros = array("Fusca","Opala","Dodge","Corcel");
+
+     foreach ( $carros as $carro)
+
+     {
+
+     echo " Meu carro é um $carro <br>";
+     
+       
+     }
+
+?> 
+
+
+  </body>
+</html>
