5a98b9a1a7ad1e22dc65c9f1b53c9038594cf724
Guilherme Pacheco Da Silva <101291879+OnlyPacheco@users.noreply.github.com>
2023-04-11 09:52:13 -0300

    Update CalculadoraPacheco.php

--- CalculadoraPacheco.php
+++ CalculadoraPacheco.php
@@ -1,32 +1,70 @@
-<!DOCTYPE HTML>
-<html lang = "pt-br">
-<?php
-
-// Definimos os valores padrões do POST:
-$_POST = array_replace(['op' => '+', 'a' => 0, 'b' => 0], $_POST);
-
-// Definimos as operações suportadas e sua função no BCMath:
-$ops = ['+' => 'bcadd', '-' => 'bcsub', '*' => 'bcmul', '/' => 'bcdiv', '**' => 'bcpow', '%' => 'bcmod'];
-
-// Encontramos a função baseado na entrada do usuário. Caso não for suportado a operação, etnão o `+` será usado como padrão:
-$op = $ops[$_POST['op']] ?? $ops['+'];   
-
-// Fazemos chamamos a função usando os valores de A e B:
-$resultado = call_user_func($op, $_POST['a'], $_POST['b'], 2);
-
-?>
-<!DOCTYPE HTML>
-<html lang="pt-br">
-<form action="#" method="post">
-    <br>A: <input name="a" type="number" step="any"
-                  value="<?= htmlentities($_POST['a'], ENT_QUOTES | ENT_HTML5, 'UTF-8') ?>">
-    <br>B: <input name="b" type="number" step="any"
-                  value="<?= htmlentities($_POST['b'], ENT_QUOTES | ENT_HTML5, 'UTF-8') ?>">
-    <br><?php
-    foreach ($ops as $n => $_) {
-        echo '<input type="submit" name="op" value="' . $n . '">';
-    }
-    ?>
-    <br> Resultado: <input readonly value="<?= $resultado ?>">
-</form>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Calculadora PHP com AJAX</title>
+    <style>
+      /* Estilos para os botões */
+      button {
+        font-size: 20px;
+        padding: 10px;
+        width: 50px;
+        height: 50px;
+        margin: 5px;
+      }
+      
+      /* Estilos para a tela de exibição */
+      #display {
+        font-size: 30px;
+        width: 200px;
+        height: 50px;
+        margin-bottom: 10px;
+      }
+    </style>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+      $(document).ready(function() {
+        // Função para enviar o formulário via AJAX
+        $("form").submit(function(e) {
+          e.preventDefault();
+          $.ajax({
+            type: "POST",
+            url: "calc.php",
+            data: $(this).serialize(),
+            success: function(response) {
+              $("#result").html(response);
+            }
+          });
+        });
+      });
+    </script>
+  </head>
+  <body>
+    <!-- Formulário para a calculadora -->
+    <form>
+      <input type="text" name="num1" id="num1" />
+      <input type="text" name="num2" id="num2" />
+      <br />
+      <button type="button" name="operator" value="+" onclick="sendForm('+')">+</button>
+      <button type="button" name="operator" value="-" onclick="sendForm('-')">-</button>
+      <button type="button" name="operator" value="*" onclick="sendForm('*')">*</button>
+      <button type="button" name="operator" value="/" onclick="sendForm('/')">/</button>
+      <button type="button" name="operator" value="√" onclick="sendForm('√')">√</button>
+      <button type="button" name="operator" value="^" onclick="sendForm('^')">^</button>
+      <button type="button" name="operator" value="sen" onclick="sendForm('sen')">sen</button>
+      <button type="button" name="operator" value="cos" onclick="sendForm('cos')">cos</button>
+      <button type="button" name="operator" value="tan" onclick="sendForm('tan')">tan</button>
+    </form>
+    
+    <!-- Tela de exibição do resultado -->
+    <div id="result"></div>
+    
+    <script>
+      function sendForm(operator) {
+        // Adiciona o operador ao formulário antes de enviá-lo via AJAX
+        $("#operator").val(operator);
+        $("form").submit();
+      }
+    </script>
+  </body>
 </html>
+
