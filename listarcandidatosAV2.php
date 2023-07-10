c5f4e047cfc1b1833c120e46eb8750047b79fba7
Guilherme Pacheco Da Silva <101291879+OnlyPacheco@users.noreply.github.com>
2023-07-10 19:04:20 -0300

    Update listarcandidatosAV2.php

--- listarcandidatosAV2.php
+++ listarcandidatosAV2.php
@@ -4,27 +4,33 @@ $username = 'OnlyPacheco';
 $password = '123';
 $dbname = 'provaAv2';
 
+// Estabelecendo a conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verificando se a conexão foi estabelecida com sucesso
 if ($conn->connect_error) {
-  die('Erro na conexão com o banco de dados , meu cria: ' . $conn->connect_error);
+  die('Erro na conexão com o banco de dados, meu cria: ' . $conn->connect_error);
 }
 
-$sql = 'SELECT * FROM candidatos ORDER BY salaprova, nome';
+// Consulta SQL para selecionar todos os candidatos, ordenados por salaprova e nome
+$sql = 'SELECT nome, cpf, identidade, email, cargo_pretendido, salaprova FROM candidatos ORDER BY salaprova, nome';
 $result = $conn->query($sql);
 
+// Verificando se existem resultados da consulta
 if ($result->num_rows > 0) {
   while ($row = $result->fetch_assoc()) {
+    // Exibindo as informações dos candidatos
     echo 'Nome: ' . $row['nome'] . '<br>';
     echo 'CPF: ' . $row['cpf'] . '<br>';
     echo 'Identidade: ' . $row['identidade'] . '<br>';
     echo 'Email: ' . $row['email'] . '<br>';
-    echo 'Cargo Que procura: ' . $row['cargo_pretendido'] . '<br>';
+    echo 'Cargo Pretendido: ' . $row['cargo_pretendido'] . '<br>';
     echo 'Sala de Prova: ' . $row['salaprova'] . '<br><br>';
   }
 } else {
-  echo 'Nenhum candidato encontrado , menó.';
+  echo 'Nenhum candidato encontrado, menó.';
 }
 
+// Fechando a conexão com o banco de dados
 $conn->close();
 ?>
